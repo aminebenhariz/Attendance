2e5f7cb553bdd7f86648f381fbccf72ac6fe88d9
Amine Ben Hariz <aminebenhariz@gmail.com>
2015-12-12 22:45:02 +0100

    Add invalid Pause creation test

--- tests/PauseTest.php
+++ tests/PauseTest.php
@@ -2,11 +2,36 @@
 
 namespace AmineBenHariz\Attendance\Tests;
 
+use AmineBenHariz\Attendance\Pause;
+
 /**
  * Class PauseTest
  * @package AmineBenHariz\Attendance\Tests
  */
 class PauseTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @dataProvider invalidPauseCreationProvider
+     * @param \DateTime $start
+     * @param \DateTime $end
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidPauseCreation(\DateTime $start, \DateTime $end)
+    {
+        new Pause($start, $end);
+    }
 
+    /**
+     * @return array
+     */
+    public function invalidPauseCreationProvider()
+    {
+        return [
+            // Pause ends before even starting! (may be possible in quantum mechanics)
+            [new \DateTime('2015-12-12 10:00'), new \DateTime('2015-12-12 09:30')],
+
+            // Pause ends the next day. Are you that lazy?
+            [new \DateTime('2015-12-12 10:00'), new \DateTime('2015-12-13 10:30')],
+        ];
+    }
 }
